cd5a8e64f999e2b7ded1739083e127b9b93f7312
davitarakelyan92 <davitarakelyan92@gmail.com>
2021-12-25 10:41:24 +0400

    Nav Bar added + major commit

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+
+  //Hamburger Menu Toggle
+  $('.nav-toggle').click(function(){
+    $('.main-nav').toggleClass('is-open');
+    $('.hamburger').toggleClass('is-open');
+  })
+
+})
