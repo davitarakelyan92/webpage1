b9e17c9820f38bb0c16cc3130a4c20b8446db21b
davitarakelyan92 <davitarakelyan92@gmail.com>
2021-12-25 15:20:45 +0400

    Major Upgrade

--- js/scripts.js
+++ js/scripts.js
@@ -4,6 +4,7 @@ $(document).ready(function(){
   $('.nav-toggle').click(function(){
     $('.main-nav').toggleClass('is-open');
     $('.hamburger').toggleClass('is-open');
+    $('body').toggleClass('no-scroll')
   })
 
 })
